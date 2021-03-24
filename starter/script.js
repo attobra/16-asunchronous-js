ee5e4198d061ad489d02c0a982d0324bc2ad809e
attobra <nora.attobra@gmail.com>
2021-03-24 14:13:05 -0400

    add country and geolocation api

--- starter/script.js
+++ starter/script.js
@@ -5,7 +5,7 @@ const countriesContainer = document.querySelector('.countries');
 ////////////////////////////////////////
 const renderError = function (msg) {
   countriesContainer.insertAdjacentText('beforeend', msg);
-  //countriesContainer.style.opacity = 1;
+  countriesContainer.style.opacity = 1;
 };
 
 ///////////////////////////////////////
@@ -31,7 +31,7 @@ const renderCountry = function (data, className = '') {
         `;
 
   countriesContainer.insertAdjacentHTML('beforeend', html);
-  //   countriesContainer.style.opacity = 1;
+  countriesContainer.style.opacity = 1;
 };
 /*
 const getCountryAndNeighbor = function (country) {
@@ -160,3 +160,35 @@ btn.addEventListener('click', function () {
 
 getCountryData('australia');
 */
+
+/////////////////////////////
+///CODING CHALLENGE #1
+
+const whereAmI = function (lat, lng) {
+  fetch(`https://geocode.xyz/${lat},${lng}?geoit=json `)
+    .then(res => {
+      if (!res.ok) throw new Error(`problem with geocoding, ${res.status}`);
+      return res.json();
+    })
+    .then(data => {
+      console.log(`You are in ${data.city}, ${data.country}`);
+      // getCountryData2(data.country);
+      return fetch(`https://restcountries.eu/rest/v2/name/${data.country}`);
+    })
+    .then(res => {
+      if (!res.ok) throw new Error(`Country not found, ${res.status}`);
+      return res.json();
+    })
+    .then(data => {
+      console.log(data[0]);
+      renderCountry(data[0]);
+    })
+    .catch(err => console.error(`${err.message} 💥`))
+    .finally(() => (countriesContainer.style.opacity = 1));
+};
+
+whereAmI(52.508, 13.381);
+whereAmI(19.037, 72.873);
+whereAmI(-33.933, 18.474);
+
+//https://geocode.xyz/53.508,13.381?geoit=json.
